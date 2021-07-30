7dbe038cb71562cc3c3e4ada618eabff172fc919
Daniel Snider <danielsnider12@gmail.com>
2021-07-30 13:12:45 +0000

    More dcgm_monitor.sh tooling

---
+++ install_dcgm_monitor.sh
@@ -0,0 +1,24 @@
+# Install DCGM 
+set -v
+set -e
+architecture=x86_64
+distribution=[YOUR OS HERE eg. "ubuntu2020"]
+echo "deb https://developer.download.nvidia.com/compute/cuda/repos/$distribution/$architecture /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/$architecture/7fa2af80.pub
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/$architecture/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-get update
+sudo apt-get install -y datacenter-gpu-manager
+sudo systemctl --now enable nvidia-dcgm
+
+# Install HFTA
+git clone https://github.com/danielsnider/hfta
+cd hfta
+pip install -e .
+
+# Stop this process because dcgm_monitor.py wants to start it
+pkill nv-hostengine
+
+# Run example dcgm_monitor
+sudo python3 run_dcgm_monitor.py
+
--- run_dcgm_monitor.py
+++ run_dcgm_monitor.py
@@ -3,11 +3,13 @@
 Example Usage:
 sudo python3 run_dcgm_monitor.py
 """
-
+import time
 from hfta.workflow.dcgm_monitor import DcgmMonitor, dcgm_monitor_start, dcgm_monitor_stop
 
 monitor = DcgmMonitor('a100')
+print('Starting...')
 monitor_thread = dcgm_monitor_start(monitor, '/home/ubuntu/dcgm_monitor_out')
+time.sleep(20) # dcgm_monitor will sample every 9 seconds.
 # Do stuff
 dcgm_monitor_stop(monitor, monitor_thread)
-
+print('Done.')
